ca7dafbebbc10b1b25ccaa5e94ba2723e50dd4c5
haruki7049 <tontonkirikiri@gmail.com>
2024-08-18 06:31:10 +0900

    test(Formatter): Init nothing_to_change

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,34 @@ struct Formatter {
 }
 
 impl Formatter {
-    fn format(code: String) -> String {
+    fn format(self) -> String {
         todo!()
+    }
+
+    pub fn new(code: String, _args: FormatterArguments) -> Self {
+        Formatter { code }
+    }
+}
+
+struct FormatterArguments {
+}
+
+impl FormatterArguments {
+    fn new() -> Self {
+        FormatterArguments {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Formatter, FormatterArguments};
+
+    #[test]
+    fn nothing_to_change() {
+        let code: String = String::from("(print 'Hello')");
+        let formatter_args: FormatterArguments = FormatterArguments::new();
+        let formatter: Formatter = Formatter::new(code, formatter_args);
+        let result: String = formatter.format();
+        assert_eq!(result, "(print 'Hello')");
     }
 }
