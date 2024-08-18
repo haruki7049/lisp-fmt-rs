bf8c57f53d9a801992c33e241a3d4074c20ae501
haruki7049 <tontonkirikiri@gmail.com>
2024-08-18 09:30:26 +0900

    chore(Formatter): Add is_sexp function

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,17 @@ impl Formatter {
         self.code
     }
 
+    /// is_sexp returns true if line's first charactor is left bracket.
+    /// ```
+    /// assert!(is_sexp(String::from("(print 'Alice')")))
+    /// assert!(!is_sexp(String::from("print 'This line is missing a left bracket')")))
+    /// assert!(!is_sexp(String::from("# This is emacs-lisp's comment")))
+    /// assert!(!is_sexp(String::from(";; This is commonlisp's comment")))
+    /// ```
+    fn is_sexp(line: String) -> bool {
+        todo!()
+    }
+
     pub fn new(code: String, _args: FormatterArguments) -> Self {
         Formatter { code }
     }
